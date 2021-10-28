974e8d2202deba2a7f4240d8b4d2972419362aa3
eduxxa <karaopap@rambler.ru>
2021-10-28 20:04:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# quasar dashboard
+
+First quasar proj
+
+### You should install yarn
+'''
+yarn
+'''
+
+### Start proj
+```bash
+quasar dev
+```
---
+++ src/pages/Error404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fullscreen bg-black text-white text-center q-pa-md flex flex-center">
+    <div>
+      <div class="text-h2" style="opacity:.4">
+        Page not found ^_^
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="black"
+        unelevated
+        to="/"
+        label="Home"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error404'
+}
+</script>
